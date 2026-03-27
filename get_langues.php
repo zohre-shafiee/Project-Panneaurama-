239157d565e2dc66d655b61829c2e761124653a9
camillebng <cbenoitguyod@gmail.com>
2026-03-27 22:43:26 +0100

    Ajout de commentaires

--- get_langues.php
+++ get_langues.php
@@ -3,11 +3,12 @@
 header('Content-Type: application/json');
 
 try {
-    // Connection à la bdd SQLite
+    // Connection à la bdd 
     $dbfile = 'gestion.db';
     $conn = new PDO("sqlite:" . $dbfile);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
+    // Récupération langue sélectionnée 
     $sql = "SELECT id_langue, nom_langue 
             FROM languages"; 
     $stmt = $conn->prepare($sql);
@@ -16,7 +17,8 @@ try {
 
     echo json_encode($langues, JSON_UNESCAPED_UNICODE);
 
-} catch(PDOException $e) { // Gestion des erreurs
+// Gestion des erreurs    
+} catch(PDOException $e) { 
     echo json_encode([
         'success' => false,
         'message' => "Erreur: " . $e->getMessage()
